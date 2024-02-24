d48b74ab242e5e775dccdcc1fb2fa83a8c225491
wilbispaulo <wilbispaulo@hotmail.com>
2024-02-23 23:27:10 -0300

    nova abordagem post/get

--- app/apipix.php
+++ app/apipix.php
@@ -4,81 +4,96 @@ use wilbispaulo\BRcode\BRcode;
 
 require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 
-header("Content-Type: application/json; charset=UTF-8");
+// header("Content-Type: application/json; charset=UTF-8");
 
-date_default_timezone_set("America/Sao_Paulo");
+// date_default_timezone_set("America/Sao_Paulo");
 
-$requestMethod = $_SERVER['REQUEST_METHOD'];
+// $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if ($requestMethod !== 'POST') {
-    header($_SERVER["SERVER_PROTOCOL"] . " 405 Method not allowed");
-    exit("Método não permitido");
-}
+// if ($requestMethod !== 'GET') {
+//     header($_SERVER["SERVER_PROTOCOL"] . " 405 Method not allowed");
+//     exit("Método não permitido");
+// }
 
-$input = file_get_contents('php://input', true);
-$input = json_decode($input, true);
+
+
+// **********************************************************************************************
+// $args is used to pass parameters from call
+// $args must be ['chave'=>'', 'nome'=>'', 'municipio'=>'', 'txId'=>'', 'valor'=>''] structure
+// ***********************************************************************************************
+
+
+
+
+
+$input = $args;
 
 // Inicializa código, mensagem e header de resposta como OK
-$cod = 200;
-$msg = "";
-$header = $_SERVER["SERVER_PROTOCOL"] . " 200 OK";
 
-if (!array_key_exists('chave_pix', $input)) {
+$statusCode = 200;
+$body = [];
+$headers = [];
+$serverProt = substr($_SERVER["SERVER_PROTOCOL"], strrpos($_SERVER["SERVER_PROTOCOL"], '/') + 1);
+
+// $header = $_SERVER["SERVER_PROTOCOL"] . " 200 OK";
+$body['msg'] = 'ok';
+
+if (!array_key_exists('chave', $input)) {
     $cod = 412;
-    $msg .= "Chave Pix ausente ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
-} elseif (!BRcode::validaChave($input['chave_pix'])) {
+    $body['msg'] = "Chave Pix ausente ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
+} elseif (!BRcode::validaChave($input['chave'])) {
     $cod = 412;
-    $msg = "Chave Pix invalida ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
+    $body['msg'] = "Chave Pix invalida ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
 }
 if (!array_key_exists('nome', $input)) {
     $cod = 412;
-    $msg .= "Nome ausente ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
+    $body['msg'] = "Nome ausente ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
 } elseif (!BRcode::validaNome($input['nome'])) {
     $cod = 412;
-    $msg .= "Nome invalido ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
+    $body['msg'] = "Nome invalido ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
 }
-if (!array_key_exists('cidade', $input)) {
+if (!array_key_exists('municipio', $input)) {
     $cod = 412;
-    $msg .= "Cidade ausente ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
-} elseif (!BRcode::validaCidade($input['cidade'])) {
+    $body['msg'] = "Município ausente ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
+} elseif (!BRcode::validaCidade($input['municipio'])) {
     $cod = 412;
-    $msg .= "Cidade invalida ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
+    $body['msg'] = "Município inválido ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
 }
-if (!array_key_exists('tx_id', $input)) {
-    $input['tx_id'] = "***";
-} elseif (!BRcode::validaTxId($input['tx_id'])) {
+if (!array_key_exists('txId', $input)) {
+    $input['txId'] = "***";
+} elseif (!BRcode::validaTxId($input['txId'])) {
     $cod = 412;
-    $msg .= "TxId invalido ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
+    $body['msg'] = "TxId invalido ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
 }
 if (!array_key_exists('valor', $input)) {
     $input['valor'] = "";
 } elseif (!BRcode::validaTxId($input['valor'])) {
     $cod = 412;
-    $msg .= "Valor invalido ";
-    $header = $_SERVER["SERVER_PROTOCOL"] . " 412 Bad request";
+    $body['msg'] = "Valor invalido ";
+    $headers['HTTP/'] = $serverProt . " 412 Bad request";
 }
 
-$resposta['codigo'] = $cod;
-$resposta['mensagem'] = $msg;
+$body['statusCode'] = $statusCode;
 
-if ($cod == 200) {
+if ($statusCode === 200) {
 
     // Cria o objeto e carrega os dados
-    $pixCode = new BRcode($input['chave_pix'], $input['nome'], $input['cidade'], $input['tx_id'], $input['valor']);
+    $pixCode = new BRcode($input['chave'], $input['nome'], $input['municipio'], $input['txId'], $input['valor']);
 
     // Chama o método para gerar a string do PIX copia e cola
     $txtPix = $pixCode->geraPixCode();
-    $resposta['txtPix'] = $txtPix;
+    $body['txtPix'] = $txtPix;
+
+    // Set headers
+    $headers['Content-Type:'] = 'application/json';
 }
 
 // Saída do response e JSON
-
-header($header);
-echo json_encode($resposta);
+// $body['']
